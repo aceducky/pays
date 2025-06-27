a9b853463ed3dce6f128ae97a061f309da964189
SrinivasD <115702816+aceducky@users.noreply.github.com>
2025-06-27 19:26:38 +0530

    fix1

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,3 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", rootRouter);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+});
